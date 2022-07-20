d8f240d5751156811e9282863cedf2b9b7d342d0
Jordi <im.jordiparadelo@gmail.com>
2022-07-20 10:06:39 +0200

    Update DotNavigation.jsx
    - Fixed useRef
    - Added InteractionObserver to set active to dots

--- frontend_react/src/components/DotNavigation/DotNavigation.jsx
+++ frontend_react/src/components/DotNavigation/DotNavigation.jsx
@@ -1,25 +1,50 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 // Styles
 import "./DotNavigation.scss";
 // Utils
-import {scrollToTarget} from "../../utils";
+import { scrollToTarget } from "../../utils";
 
 const navLinks = [
   { name: "Home", url: "Home" },
   { name: "About", url: "About" },
-  { name: "Contact", url: "Contact" },
+  { name: "Contact", url: "Footer" },
   { name: "Work", url: "Works" },
   { name: "Skills", url: "Skills" },
 ];
 
 const DotNavigation = () => {
-  const [activeLink, setActiveLink] = useState(null)
-  const linkRef = useRef(navLinks.map(() => React.createRef()))
+  const [activeLink, setActiveLink] = useState(null);
+  const navLinksRef = useRef(navLinks.map(() => React.createRef()));
+  // const navLinksRef = useRef(navLinks);
 
   function handleLinkClick(event) {
-    scrollToTarget(event)
-    setActiveLink(event.currentTarget)
+    scrollToTarget(event);
   }
+
+  useEffect(() => {
+    const sectionsColletion = navLinksRef.current.map(({ current }) => {
+      const id = current.hash
+      return document.querySelector(id)
+    });
+
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const { target } = entry;
+          const activeLink = navLinksRef.current.find(({ current }) =>
+            current.hash.includes(target.id)
+          );
+          activeLink.current.classList.toggle("active", entry.isIntersecting);
+        });
+      },
+      {
+        threshold: [0.2, 0.5],
+      }
+    );
+
+    // // Listeners
+    sectionsColletion.forEach((section) => sectionObserver.observe(section));
+  }, []);
 
   return (
     <nav className="dot-navigation">
@@ -27,9 +52,11 @@ const DotNavigation = () => {
         <a
           href={`#${link.url}`}
           key={`dot-link-${index}`}
-          className={`dot-navigation__link ${activeLink == linkRef.current[index].current ? "active" : ""}`}
+          className={`dot-navigation__link ${
+            activeLink == navLinksRef.current[index].current ? "active" : ""
+          }`}
           onClick={handleLinkClick}
-          ref={linkRef.current[index]}
+          ref={navLinksRef.current[index]}
         ></a>
       ))}
     </nav>
