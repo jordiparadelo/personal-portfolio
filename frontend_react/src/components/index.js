45f65094f7c7aad1caefd488e2c5f1baa454514f
Jordi <im.jordiparadelo@gmail.com>
2022-07-22 11:34:11 +0200

    ContactForm Component
    - New component added
    - Imported to Footer

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as SocialMedia} from './SocialMedia/SocialMedia'
 export {default as DotNavigation} from './DotNavigation/DotNavigation'
 export {default as Navbar} from './Navbar/Navbar'
+export {default as ContactForm} from './ContactForm/ContactForm'
--- frontend_react/src/container/Footer/Footer.js
+++ frontend_react/src/container/Footer/Footer.js
@@ -1,52 +1,10 @@
 import React, { useState } from "react";
 // Styles
 import "./Footer.scss";
-// Data
-import { client } from "../../clients";
-// Lib
-import { formValidator } from "../../utils";
-import { useForm } from "react-hook-form";
+// Components
+import { ContactForm } from "../../components";
 
 const Footer = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-
-  const { name, email, message } = formData;
-
-  // Methods
-  function handleChangeInput(e) {
-    const { name, value } = e.target;
-
-}
-
-function onSubmit(data) {
-    if(!data) return;
-    console.log(data)
-    setFormData({ ...formData, ...data });
-    
-    const contact = {
-        _type: "contact",
-        name: name,
-        email: email,
-        message: message,
-      };
-
-      client.create(contact).then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      });
-  }
-
   return (
     <footer id="Footer">
       <div className="app__wrapper">
@@ -59,52 +17,7 @@ function onSubmit(data) {
             form
           </p>
         </header>
-
-        {isFormSubmitted ? (
-          <h3>Thanks you for getting in touch!</h3>
-        ) : (
-          <form
-            className="footer__contact-form"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="input-group">
-              <label htmlFor="name">What's your name?</label>
-              <input
-                type="text"
-                {...register("name", { required: true, minLength: 3 })}
-                onChange={handleChangeInput}
-                data-required
-              />
-
-              {errors.name?.type.includes("required") && (
-                <div className="status-message">Please enter your name</div>
-              )}
-            </div>
-            <div className="input-group">
-              <label htmlFor="email">Contact email</label>
-              <input
-                type="text"
-                {...register("email", { required: true, pattern: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i })}
-                data-required
-              />
-              {errors.email?.type.includes("required") && (
-                <div className="status-message">Please enter your email</div>
-              )}
-              {errors.email?.type.includes("pattern") && (
-                <div className="status-message">There is a problem in your email format</div>
-              )}
-            </div>
-            <textarea
-              {...register("message")}
-              cols="30"
-              rows="10"
-              placeholder="Tell me about your proyect"
-            ></textarea>
-            <button className="primary">
-              {loading ? "Sending..." : "Send Preposition"}
-            </button>
-          </form>
-        )}
+          <ContactForm/>
       </div>
     </footer>
   );
