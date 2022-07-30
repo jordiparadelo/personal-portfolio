c3c2d27f8f07b0263e09e8d8072823956b646604
Jordi <im.jordiparadelo@gmail.com>
2022-07-30 11:30:19 +0200

    Clean Up

---
+++ frontend_react/src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <div className="app__wrapper">Projects</div>
+        </div>
+    );
+};
+
+export default Projects;
