749746fe0e91cf6e3875ed54db8bc66c2506e99d
Jordi <im.jordiparadelo@gmail.com>
2023-04-29 22:32:23 +0200

    react router updated

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -5,27 +5,44 @@ import { Footer } from "container";
 // Styles
 import "App.scss";
 //lib
-import { Routes, Route } from "react-router-dom";
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+} from "react-router-dom";
 import { Home, Portfolio, ProjectPage } from "pages";
 
 const App = () => {
-  useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }, []);
+	useEffect(() => {
+		let vh = window.innerHeight * 0.01;
+		// Then we set the value in the --vh custom property to the root of the document
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+	}, []);
 
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/portfolio/:projectId" element={<ProjectPage />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<BrowserRouter>
+				<Switch>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='/portfolio'
+						element={<Portfolio />}
+					/>
+					<Route
+						path='/portfolio/:projectId'
+						element={<ProjectPage />}
+					/>
+				</Switch>
+			</BrowserRouter>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
